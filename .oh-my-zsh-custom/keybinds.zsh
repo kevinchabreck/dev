4af7989b35e7e2e2addb2be12e4c5e251b514a4d
Kevin Chabreck <kevin.chabreck@gmail.com>
2016-08-11 09:54:28 -0400

    updated keybinds

--- .oh-my-zsh-custom/keybinds.zsh
+++ .oh-my-zsh-custom/keybinds.zsh
@@ -1,15 +1,13 @@
-# don't forget to add iterm keybinds to send the following escapse sequences:
-# 	copy
-# 	backward-kill-line
+# don't forget to add iterm keybinds to send the following escape sequences:
+# 	copyline
 #
 # ex: http://superuser.com/questions/349439/how-to-bind-command-key-in-zsh
 
 function copyline {
+	zle copy-region-as-kill ""
 	zle vi-yank-whole-line
 	echo -n $CUTBUFFER | pbcopy
 }
-
 zle -N copyline
 
-bindkey '^[copy' copyline
-bindkey '^[kill-line-backward' backward-kill-line
+bindkey '^[copyline' copyline
